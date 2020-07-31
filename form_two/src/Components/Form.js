1eab4e1e9410aff282343567b63a83835e78dbd5
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-07-31 18:02:00 -0500

    update

---
+++ form_two/src/Components/Form.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'reat'
+
+class Form extends Component {
+    constructor() {
+        super()
+        this.state = {
+            
+        }
+    }
+
+
+
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Form; 
