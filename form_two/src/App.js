116ec7dcc618d8f31c57d3bd544f8e830921a6d2
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-01 20:46:00 -0500

    update

--- form_two/src/App.js
+++ form_two/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Form from './Components/Form';
 
 
 
@@ -8,7 +9,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <Form/>
     </div>
   );
 }
--- form_two/src/Components/Form.js
+++ form_two/src/Components/Form.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'reat'
+import React, {Component} from 'react'
 
 class Form extends Component {
     constructor() {
@@ -8,18 +8,63 @@ class Form extends Component {
             password: '',
             age: '',
             gender: '',
-            ocupation: ''
+            occupation: ''
         }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleAgeChange = this.handleAgeChange.bind(this);
+        this.handleGenderChange = this.handleGenderChange.bind(this);
+        this.handleOcupationChange = this.handleOcupationChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleNameChange(event) {
+        this.setState({username: event.target.value});
+    }
 
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
 
+    handleAgeChange(event) {
+        this.setState({age: event.target.value});
+    }
+
+    handleGenderChange(event) {
+        this.setState({gender: event.target.value})
+    }
+
+    handleOcupationChange(event) {
+        this.setState({ocupation: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('User Information was submitted: ');
+        event.preventDefault();
+    }
 
     render(){
         return(
-            <div>
-
-            </div>
+        <div className='container'>
+            <form ClassName='form' onSubmit={this.handleSubmit}>
+                <input placeholder='username' type='text' value={this.state.username} onChange={this.handleNameChange}/>
+            </form>
+            <form>
+                <input placeholder='password' type='password' value={this.state.password} onChange={this.handlePasswordChange}/>
+            </form>  
+            <form>
+                <input placeholder='age' type='text' value={this.state.age} onChange={this.handleAgeChange}/>
+            </form>
+                <input className='male' type='radio' name='gender' value='male' onChange={this.handleGenderChange}/>
+                <label for='male'>Male</label>
+                <input className='male' type='radio' name='gender' value='female' onChange={this.handleGenderChange}/>
+                <label for='female'>Female</label>
+            <form>
+                <input placeholder='occupation' type='text' value={this.state.ocupation} onChange={this.handleOcupationChange}/>
+                <br/><br/>
+                <button className='submit' type='submit' value='Submit'>Submit</button> 
+            </form>
+        </div>
         );
     }
 }
